05a13cc14ef6a20377c811ec72187ef64a6b7b9c
pboopathi <pboopathi@ebay.com>
2020-10-25 12:07:04 -0700

    Added Maven Dependency

--- src/main/java/browserStackSetup/BrowserStackSetup.java
+++ src/main/java/browserStackSetup/BrowserStackSetup.java
@@ -1,4 +1,14 @@
 package browserStackSetup;
 
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class BrowserStackSetup {
+
+    WebDriver driver = new ChromeDriver();
+// driver.manage().window().maximize();
+// driver.manage().timeouts().pageLoadTimeout(1,TimeUnit.SECONDS);
+//  driver.get("http://learn-automation.com/");
 }
