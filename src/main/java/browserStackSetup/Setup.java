22bc3ca6bc079500707d094cfb6d2b2de96ceea2
pboopathi <pboopathi@ebay.com>
2020-10-26 21:05:09 -0700

    Log4j code added

--- src/main/java/browserStackSetup/Setup.java
+++ src/main/java/browserStackSetup/Setup.java
@@ -3,6 +3,7 @@ package browserStackSetup;
 
 import browserStackPages.BrowserStackHomePage;
 import browserStackPages.BrowserStackSignUpPage;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,16 +11,25 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import org.apache.logging.log4j.LogManager;
 
 
 public class Setup {
 
-    public   static   WebDriver driver;
+    public static WebDriver driver;
     public BrowserStackHomePage browserStackHomePage;
     public BrowserStackSignUpPage browserStackSignUpPage;
+    public static Logger LOGGER = null;
+
+
+    public  Setup(){
+        LOGGER = LogManager.getLogger(Setup.class);
+        LOGGER.info("Test completed");
+    }
 
     @BeforeTest
-    public void setUp() throws MalformedURLException, InterruptedException {
+    public void setDriver() throws MalformedURLException, InterruptedException {
+        LOGGER.info("Chrome Driver Setup");
         System.setProperty("webdriver.chrome.driver", "/Users/pboopathi/Downloads/chromedriver" );
         browserStackHomePage =  new BrowserStackHomePage(driver);
         browserStackSignUpPage = new BrowserStackSignUpPage(driver);
